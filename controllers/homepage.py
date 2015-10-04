f35c11c6080338b52c42e8c2b3199e9000b95b30
Alex LaFroscia <alex@lafroscia.com>
2015-10-04 16:43:09 -0400

    Make the user_required decorator syntax cleaner

--- controllers/homepage.py
+++ controllers/homepage.py
@@ -1,12 +1,10 @@
-import logging
 import webapp2
-from decorators.user_required import user_required
+from decorators import user_required
 
 
 class MainHandler(webapp2.RequestHandler):
 
     @user_required
     def get(self):
-        logging.info(self.auth.user)
         self.response.write('Hello, ' + self.auth.user.nickname() + '<br />')
         self.response.write('<a href="' + self.auth.logout_url + '">Logout</a>')
