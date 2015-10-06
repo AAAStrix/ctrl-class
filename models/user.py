8d483a50379dc5e3f7b61e080c898b1cd758261d
Audrey Ann Blakely <aub7@pitt.edu>
2015-10-06 03:06:14 -0400

    Minor indentation edits
    (no description necessary)

--- models/user.py
+++ models/user.py
@@ -2,11 +2,11 @@ from google.appengine.ext import ndb
 
 
 class User(ndb.Model):
-    email = ndb.StringProperty()
-    courses = ndb.StringProperty(repeated=True)
+	email = ndb.StringProperty()
+	courses = ndb.StringProperty(repeated=True)
 
-    @classmethod
-    def get_from_authentication(self, google_user):
+	@classmethod
+	def get_from_authentication(self, google_user):
         """Get or create a user based on the ID retrieved from Google"""
         user_id = google_user.user_id()
         user = User.get_by_id(user_id)
