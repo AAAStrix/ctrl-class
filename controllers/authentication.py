3012fbcc5540cf404e387f808019f0926a1e23f8
Audrey Ann Blakely <aub7@pitt.edu>
2015-10-07 13:44:34 -0400

    calling render_template
    Commented out, would like to call render_template from view

--- controllers/authentication.py
+++ controllers/authentication.py
@@ -3,8 +3,8 @@
 import webapp2
 from google.appengine.api import users
 
-
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
         login_url = users.create_login_url()
-        self.response.write('<a href="' + login_url + '">Log in!</a>')
+        # render_template(self, 'login.html', {})
+        self.response.write('<a href="' + login_url + '">Log in!</a')
--- controllers/homepage.py
+++ controllers/homepage.py
@@ -6,5 +6,6 @@ class MainHandler(webapp2.RequestHandler):
 
     @user_required
     def get(self):
+        # render_template(self, 'home.html', {})
         self.response.write('Hello, ' + self.auth.user.nickname() + '<br />')
         self.response.write('<a href="' + self.auth.logout_url + '">Logout</a>')
