d0d1e18d38a357e77d37723a864f66594d1a39f9
Alex LaFroscia <alex@lafroscia.com>
2015-10-07 15:29:47 -0400

    Add user info to template automatically

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,6 +3,14 @@ from google.appengine.ext.webapp import template
 
 
 def render_template(handler, templatename, templatevalues={}):
+    # Copy the original hash to avoid mutation and add the authentication
+    # information to it
+    values = templatevalues.copy()
+    if hasattr(handler, 'auth'):
+        values.update({
+            'auth': handler.auth
+        })
+
     path = os.path.join(os.path.dirname(__file__), '../views/' + templatename)
-    html = template.render(path, templatevalues)
+    html = template.render(path, values)
     handler.response.out.write(html)
