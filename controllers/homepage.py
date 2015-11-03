edfe03e26cf7ee34903ab5c3f97c2a2ae210f5cf
Audrey Ann Blakely <aub7@pitt.edu>
2015-11-02 23:21:59 -0500

    Sends email to user on start up

--- controllers/homepage.py
+++ controllers/homepage.py
@@ -1,5 +1,6 @@
 import webapp2
 from utils.decorators import user_required
+from google.appengine.api import mail
 from utils import render_template
 
 
@@ -11,3 +12,14 @@ class MainHandler(webapp2.RequestHandler):
             'courses': self.auth.user.courses
         }
         render_template(self, 'home.html', params)
+        
+    @user_required
+    def post(self):
+        from_address = 'contact@ctrl-class.appspotmail.com'
+        subject = 'Contact from ctrl-class'
+        body = 'Welcome to ctrl-class! Take control over your courses and projects.\n\
+                 Questions? Contact us at contact@ctrl-class.appspotmail.com. \n\n\
+                 -The ctrl-class team'
+        mail.send_mail(from_address, self.auth.user.email, subject, body)
+
+        
