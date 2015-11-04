f1a53271b1cde600665d0443b5e5b9aaf6979d32
Alex LaFroscia <alex@lafroscia.com>
2015-11-04 15:22:58 -0500

    Allow checking off tasks

--- controllers/tasks.py
+++ controllers/tasks.py
@@ -2,17 +2,20 @@ import webapp2
 from utils.decorators import user_required
 from utils import render_template, render_json
 from models.task import Task
-from models.project import Project	
-		
+from models.project import Project
+
+
 class TaskHandler(webapp2.RequestHandler):
-	
-	def post(self):
-		task_token = self.request.get('key')
-		task = Task.find_with_key(task_token)
-		task.completed == true
-		task.put()
-		render_json(self)
-		
-		
-		
-		
+
+    @user_required
+    def post(self):
+        """
+        Toggle completion of a task
+
+        Takes a task, inverts the completion and saves it
+        """
+        task_token = self.request.get('key')
+        task = Task.find_with_key(task_token)
+        task.completed = not task.completed
+        task.put()
+        render_json(self, obj=task.as_json())
