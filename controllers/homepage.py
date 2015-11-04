a080c38ee57548faa4b111d559140f70a5c68f8a
Alex LaFroscia <alex@lafroscia.com>
2015-11-03 19:10:39 -0500

    Allow the homepage to (theoretically) dynamically render projects

--- controllers/homepage.py
+++ controllers/homepage.py
@@ -8,11 +8,13 @@ class MainHandler(webapp2.RequestHandler):
 
     @user_required
     def get(self):
+        project_json = map(lambda x: x.as_json(), self.auth.user.projects)
         params = {
-            'courses': self.auth.user.courses
+            'courses': self.auth.user.courses,
+            'project_json': project_json
         }
         render_template(self, 'home.html', params)
-        
+
     @user_required
     def post(self):
         from_address = 'contact@ctrl-class.appspotmail.com'
