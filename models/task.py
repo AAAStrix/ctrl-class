66f17c85178a774cf9f248c50cbfb90535c9b419
Alex LaFroscia <alex@lafroscia.com>
2015-11-04 00:48:52 -0500

    Clean up Task model, mostly just fixing whitespace
    Python, by convention, uses 4 spaced per indentation level.  It seemed
    like the file was using some combination of tabs and spaces or
    something, so I just replaced it with the right number of spaced.

--- models/task.py
+++ models/task.py
@@ -3,14 +3,17 @@ from google.appengine.ext import ndb
 
 class Task(ndb.Model):
 
-	title = ndb.StringProperty()
-	completed = ndb.BooleanProperty()
-		
-	def as_json(self):
-		obj = {
-			'key': self.key.urlsafe(),
-			'title': self.title
-			'completed': self.completed
-		}
-		return obj
+    title = ndb.StringProperty()
+    completed = ndb.BooleanProperty()
 
+    def as_json(self):
+        obj = {
+            'key': self.key.urlsafe(),
+            'title': self.title,
+            'completed' : self.completed
+        }
+        return obj
+
+    @classmethod
+    def find_with_key(cls, token):
+        return ndb.Key(urlsafe=key).get()
