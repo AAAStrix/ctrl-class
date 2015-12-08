6a16aa066810cffde2fe4d4a8ff71a92d6ee8631
Alex LaFroscia <alex@lafroscia.com>
2015-12-08 16:41:51 -0500

    Add action handling for removed tasks

--- assets/js/components/project-task-list.js
+++ assets/js/components/project-task-list.js
@@ -42,6 +42,19 @@ class ActionDispatch {
         }
       });
   }
+
+  /**
+   * Send the request to the server to remove a task, then update the store
+   */
+  removeTask(id) {
+    const task = this.store.tasks[id];
+    const taskKey = task.key;
+    const projectKey = task.project.key;
+    $.get(`/task/remove?task_key=${taskKey}&project_key=${projectKey}`)
+      .done(() => {
+        this.store.update(taskKey, { removed: true });
+      });
+  }
 }
 
 let storeInstance = null;
@@ -119,41 +132,74 @@ class TaskItem extends React.Component {
     });
   }
 
-  handleChange(event) {
+  completeTask(event) {
     const checked = event.target.checked;
     this.dispatch.updateCompletedState(this.state.task.key, checked);
   }
 
+  removeTask() {
+    if (confirm('Are you sue you want to move this task?')) {
+      this.dispatch.removeTask(this.state.task.key);
+    }
+  }
+
   render() {
     const { task } = this.state;
-    const checked = task.completed;
-    const clickHandler = this.handleChange.bind(this);
+    const { completed } = task;
+    const completeHandler = this.completeTask.bind(this);
+    const removeHandler = this.removeTask.bind(this);
     const style = {};
-    if (checked) {
+    if (completed) {
       style['text-decoration'] = 'line-through';
     } else {
       style['text-decoration'] = 'none';
     }
     return (
       <li className='task'>
-        <input type='checkbox' onChange={clickHandler} checked={checked} />
+        <input type='checkbox' onChange={completeHandler} checked={completed} />
         <span style={style}>{task.title}</span>
-		&nbsp;
-		<span style={style}>{task.dueDate}</span>
-		Due:
-		<a href="/task/remove?key={{ this.props.task.key }}&project={{ this }}">Remove</a>
+        &nbsp;
+        <span style={style}>{task.dueDate}</span>
+        Due: <a href='#' onClick={removeHandler}>Remove</a>
       </li>
     );
   }
 }
 
 class TaskList extends React.Component {
-  render() {
-    const project = this.props.project;
-    this.props.tasks = parseStringArray(this.props.tasks);
-    let tasks = this.props.tasks.map((task) => {
-      return <TaskItem task={task} project={project}/>;
+  constructor(props) {
+    super(props);
+    this.store = Store.getInstance();
+    let { tasks, project } = props;
+    tasks = parseStringArray(tasks);
+    tasks.forEach((task) => {
+      const { removed } = task;
+      this.store.registerForChanges(task.key, (state) => {
+        if (removed !== state.removed) {
+          this.updateStateFor(state);
+        }
+      });
     });
+    this.state = { tasks, project };
+  }
+
+  updateStateFor(updatedTask) {
+    const tasks = this.state.tasks.map(function(task) {
+      if (task.key === updatedTask.key) {
+        task.removed = updatedTask.removed;
+      }
+      return task;
+    });
+    this.setState({ tasks });
+  }
+
+  render() {
+    const project = this.state.project;
+    let tasks = this.state.tasks
+      .filter((task) => !task.removed)
+      .map((task) => {
+        return <TaskItem task={task} project={project}/>;
+      });
     if (tasks.length === 0) {
       tasks = <EmptyPlaceholder>No Tasks</EmptyPlaceholder>;
     }
