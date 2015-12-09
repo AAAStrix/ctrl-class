f65501ffe65dba8254ecbff252a9c993ec113f55
Alex LaFroscia <alex@lafroscia.com>
2015-12-09 16:30:59 -0500

    Update seed course data

--- controllers/admin.py
+++ controllers/admin.py
@@ -4,8 +4,13 @@ import webapp2
 from models.course import Course
 
 names = [
+    'CS 445: Data Structures',
+    'CS 447: Computer Organization and Assembly Language',
     'CS 1520: Web Applicaton Development',
     'CS 1550: Introduction to Operating Systems',
+    'CS 1571: Introduction to Artificial Intelligence',
+    'CS 1632: Software Quality Assurance',
+    'CS 2730: Introduction to Natural Language Processing',
     'MATH 1000: Introduction to Statistics'
 ]
 
